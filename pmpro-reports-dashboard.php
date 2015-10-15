9812c99efabcdee4732efa76b1d60ab2dda6d1d0
Jason Coleman <jason@strangerstudios.com>
2015-10-15 15:28:40 -0400

    style update for mobile

--- pmpro-reports-dashboard.php
+++ pmpro-reports-dashboard.php
@@ -18,11 +18,11 @@ function init_pmpro_reports_dashboard()
 		<link rel="stylesheet" id="googleFonts-css" href="//fonts.googleapis.com/css?family=Lato:400,700&ver=4.3.1" type="text/css" media="all">
 		<style>
 			body {background: #FAFAFA; color: #404040; font-family: 'Lato', sans-serif; font-weight: 400; font-size: 16px; font-size: 1.6rem; line-height: 2.6rem; margin: 0; padding: 0; }
-			div {background: #FAFAFA; border-bottom: 5px solid #EEE; padding: 2rem 1rem; }
+			div {background: #FAFAFA; border-bottom: 5px solid #EEE; padding: 2rem .5rem; }
 			table {border: 1px solid #EEE; border-collapse: separate; border-spacing: 0; width: 100%; }
-			td, th {font-size: 16px; font-size: 1.6rem; line-height: 2.6rem; padding: 1rem; text-align: left; }
-			thead th {background: #EEE; }
-			tbody td, tbody th {border-top: 1px solid #EEE; }
+			thead th {background: #EEE; font-size: 12px; font-size: 1.2rem; line-height: 2rem; padding: 1rem .5rem; text-align: left; }
+			tbody th {border-top: 1px solid #EEE; font-size: 12px; font-size: 1.2rem; line-height: 2rem; padding: 1rem .5rem; text-align: left; }
+			tbody td {border-top: 1px solid #EEE; font-size: 14px; font-size: 1.4rem; line-height: 2.4rem; padding: 1rem .5rem; text-align: left; }
 			tbody tr:nth-child(odd) td, tbody tr:nth-child(odd) th {background: #FFF; }
 			h2 {color: #AAA; font-size: 18px; font-size: 1.8rem; font-weight: 300; letter-spacing: 1px; margin: 0 0 1rem 0; padding: 0; text-transform: uppercase; }
 			#pmpro_report_sales thead th:last-child {text-align: right; }
