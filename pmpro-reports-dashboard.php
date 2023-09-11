ad3b731335e78106e6ed926930ba3aa539885dd8
Andrew Lima <andrew@arctek.co.za>
2023-09-11 11:56:15 +0200

    Merge dev into PWA branch

--- pmpro-reports-dashboard.php
+++ pmpro-reports-dashboard.php
@@ -10,6 +10,9 @@ Text Domain: pmpro-reports-dashboard
 Domain Path: /languages
 */
 
+
+define( 'PMPRORD_DIR', plugin_dir_path( __FILE__ ) );
+
 /**
  * Load the languages folder for translations.
  */
@@ -71,7 +74,93 @@ function init_pmpro_reports_dashboard()
 		exit;
 	}
 }
-add_action('init', 'init_pmpro_reports_dashboard');
+add_action('init', 'init_pmpro_reports_dashboard')
+
+/**
+ * Figure out if we are loading the reports dashboard.
+ */
+function pmprord_controller()
+{
+	global $wp_filesystem;
+	
+	if ( empty( $_REQUEST['pmpro_reports_action'] ) && strpos( $_SERVER['REQUEST_URI'], '/pmpro-reports-dashboard' ) !== 0 ) {
+		return;
+	}
+	
+	//if ( ! current_user_can( 'administrator' ) && ! current_user_can( 'pmpro_membership_manager' ) ) {
+	//	return;
+	//}
+	
+	$action = get_query_var( 'pmpro_reports_action' );
+	
+	require_once ( ABSPATH . '/wp-admin/includes/file.php' );
+	WP_Filesystem();
+	
+	switch( $action ) {
+		case 'sw':
+			// load the SW JS
+			header('Content-Type: application/javascript');			
+			echo $wp_filesystem->get_contents( PMPRORD_DIR . '/js/sw.js' );
+			break;
+		case 'manifest':
+			// load the PWA manifest
+			header('Content-Type: application/json');			
+			echo $wp_filesystem->get_contents( PMPRORD_DIR . '/manifest.json' );
+			break;
+		case 'icon180':
+			// load the app icon
+			header('Content-Type: image/png');
+			echo $wp_filesystem->get_contents( PMPRORD_DIR . '/images/icon-180.png' );
+			break;
+		case 'icon750':
+			// load the app icon
+			header('Content-Type: image/png');
+			echo $wp_filesystem->get_contents( PMPRORD_DIR . '/images/icon-750.png' );
+			break;
+		default:
+			include( 'includes/dashboard.php' );
+	}
+	
+	exit;
+}
+add_action('template_redirect', 'pmprord_controller');
+
+/**
+ * Add our query vars
+ */
+function pmprord_query_vars( $vars ){
+	$vars[] = 'pmpro_reports_action';	
+	return $vars;
+}
+add_filter( 'query_vars', 'pmprord_query_vars', 10, 1 );
+
+/**
+ * Add shorter rewrite endpoint for the dashboard.
+ */
+function pmprordb_add_rewrite_rule() {	
+	add_rewrite_rule( '^pmpro-reports-dashboard/sw.js$', 'index.php?pmpro_reports_action=sw', 'top' );
+	add_rewrite_rule( '^pmpro-reports-dashboard/manifest.json$', 'index.php?pmpro_reports_action=manifest', 'top' );
+	add_rewrite_rule( '^pmpro-reports-dashboard/images/icon-180.png$', 'index.php?pmpro_reports_action=icon180', 'top' );
+	add_rewrite_rule( '^pmpro-reports-dashboard/images/icon-750.png$', 'index.php?pmpro_reports_action=icon750', 'top' );
+	add_rewrite_rule( '^pmpro-reports-dashboard', 'index.php?pmpro_reports_action=dashboard', 'top' );
+	
+	flush_rewrite_rules();
+}
+add_action( 'init', 'pmprordb_add_rewrite_rule' );
+
+/**
+ * Keep WP from adding an ending slash
+ * to our ../sw.js URL
+ */
+function pmprordb_redirect_canonical_callback( $redirect_url, $requested_url ) {
+	$action = get_query_var( 'pmpro_reports_action' );	
+	if ( in_array( $action, array( 'sw', 'manifest', 'icon' ) ) ) {
+		return $requested_url;
+	}
+	
+	return $redirect_url;
+}
+add_filter( 'redirect_canonical', 'pmprordb_redirect_canonical_callback', 100, 2 );
 
 /**
  * Add links to the plugin action links
